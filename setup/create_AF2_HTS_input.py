a537d0248a51fccc298b01491bfc164264e5d798
Jan Stuke <jan.stuke@biophys.mpg.de>
2024-09-05 13:59:47 +0000

    Update create_AF2_HTS_input.py, added license

--- setup/create_AF2_HTS_input.py
+++ setup/create_AF2_HTS_input.py
@@ -4,7 +4,23 @@
 This script creates bait and candidate input files for the AlphaPulldown pipeline (https://doi.org/10.1093/bioinformatics/btac749). It takes fasta-files and fragmentation instructions as input and outputs fasta and txt files.
 """
 
-#Version 2024-08-30, Jan Stuke
+#    This script is part of af2_lir_screen.
+#    Copyright (C) 2024  Jan Stuke
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#Version 2024-08-30
 
 import math
 
