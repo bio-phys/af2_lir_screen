17e5b8bbbb8778c82bd4aa53cfa6526b20a86692
Jan Stuke <jan.stuke@biophys.mpg.de>
2024-09-05 13:59:11 +0000

    Update seq_scan_class.py, added license

--- analysis/seq_scan_class.py
+++ analysis/seq_scan_class.py
@@ -1,5 +1,22 @@
 #Python functions to analyze the output from a fragment AlphaFold2 scan via Alphapulldown with the goal of identifying LIRs and otehr SLiMs
-#Version 2024-08-30, Jan Stuke
+
+#    This script is part of af2_lir_screen.
+#    Copyright (C) 2024  Jan Stuke
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+#Version 2024-08-30
 
 import numpy as np
 import matplotlib.pyplot as plt
